86870270246dc2e2d56553babb88e6c5dd73cf8f
Rodrigo Miguel Neto Jerónimo <jeronimorodrigo04@gmail.com>
2024-10-08 23:30:29 +0100

    Página LOGIN terminada+ criação da página inicial

--- LupsyApp/app/src/main/java/pt/ipleiria/estg/dei/lupsyapp/LoginActivity.java
+++ LupsyApp/app/src/main/java/pt/ipleiria/estg/dei/lupsyapp/LoginActivity.java
@@ -1,7 +1,11 @@
 package pt.ipleiria.estg.dei.lupsyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private EditText etEmail;
+    private EditText etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +30,46 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+    }
+    private boolean isEmailValido(String email){
+        if(email == null)
+            return false;
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+
+    }
+    private boolean isPasswordValida(String password){
+        if(password == null)
+            return false;
+        return password.length() >= 4;
     }
 
+
+
     public void onClickLogin(View view) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        // Validar email e senha
+        if (!isEmailValido(email)) {
+            etEmail.setError(getString(R.string.email_invalido));
+            return;
+        }
+
+        if (!isPasswordValida(password)) {
+            etPassword.setError(getString(R.string.password_invalida));
+            return;
+        }
+
+        // Aqui você pode adicionar o restante do código de login
+        Toast.makeText(this, "Login bem-sucedido", Toast.LENGTH_SHORT).show();
+
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
--- LupsyApp/app/src/main/java/pt/ipleiria/estg/dei/lupsyapp/MainActivity.java
+++ LupsyApp/app/src/main/java/pt/ipleiria/estg/dei/lupsyapp/MainActivity.java
@@ -1,24 +1,39 @@
 package pt.ipleiria.estg.dei.lupsyapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView tvData = findViewById(R.id.tvData);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd 'de' MMMM 'de' yyyy", new Locale("pt", "BR"));
+
+        String dataAtual = dateFormat.format(calendar.getTime());
+
+        tvData.setText(dataAtual);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.show(); // Exibe a ActionBar
+        }
     }
 }
