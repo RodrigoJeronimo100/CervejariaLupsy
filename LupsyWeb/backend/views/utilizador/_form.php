1daf7c2c4ef870a7f8899b6bf75bbe6b7d4b702a
Rodrigo Miguel Neto Jerónimo <jeronimorodrigo04@gmail.com>
2024-10-29 23:11:54 +0000

    Utilizador Frontend, Backend

---
+++ LupsyWeb/backend/views/utilizador/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\Utilizador $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="utilizador-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nif')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'telefone')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'morada')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'id_user')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
