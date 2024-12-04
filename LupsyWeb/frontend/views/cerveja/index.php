104941fd8e4aa65d0be152ed8acc86a583aea53f
Rodrigo Miguel Neto Jerónimo <jeronimorodrigo04@gmail.com>
2024-12-04 20:31:03 +0000

    estetica css

--- LupsyWeb/frontend/views/cerveja/index.php
+++ LupsyWeb/frontend/views/cerveja/index.php
@@ -6,6 +6,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
+use yii\widgets\Breadcrumbs;
 use yii\widgets\ListView;
 
 /** @var yii\web\View $this */
@@ -15,6 +16,17 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="cerveja-index">
     <br><br><br>
+    <?= Breadcrumbs::widget([
+        'homeLink' => ['label' => 'Home', 'url' => ['/site/index']],
+        'links' => [
+            ['label' => 'Cervejas', 'url' => null],
+        ],
+        'options' => ['class' => 'breadcrumb', 'style' => 'background-color: #f7f5f0; margin-top: -8px; margin-bottom: -8px;'],
+        'itemTemplate' => "<li style='display: inline-block; margin-right: 5px;'>{link}</li> &nbsp; > &nbsp; ",
+        'activeItemTemplate' => "<li style='display: inline-block; margin-right: 5px; font-weight: bold;'>{link}</li>",
+    ]); ?>
+    <h1><?= Html::encode($title = 'Cervejas') ?></h1>
+    <br>
     <!-- Filtro de Categoria -->
     <div class="filtro-categoria mb-3">
         <?php $form = ActiveForm::begin([
@@ -49,6 +61,29 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 </div>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap');
+    h1{
+        font-family: "Space Grotesk", sans-serif;
+        display: flex;
+        justify-content: center;
+        padding-left: 15px;
+        padding-right: 15px;
+        font-weight: bold;
+    }
+    .breadcrumb {
+        font-size: 14px;
+        color: #333;
+    }
+    .breadcrumb a {
+        text-decoration: none;
+        color: #373737;
+    }
+    .breadcrumb a:hover {
+        text-decoration: none;
+    }
+</style>
+
 
 
 
--- LupsyWeb/frontend/views/historico-bebi/index.php
+++ LupsyWeb/frontend/views/historico-bebi/index.php
@@ -5,6 +5,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
+use yii\widgets\Breadcrumbs;
 
 /** @var yii\web\View $this */
 /** @var yii\data\ActiveDataProvider $dataProvider */
@@ -13,8 +14,18 @@ $this->title = 'Cervejas Bebidas';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <?php $this->registerCssFile("@web/css/index_historico.css"); ?>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 <div class="historico-bebi-index">
 
+    <?= Breadcrumbs::widget([
+        'homeLink' => ['label' => 'Home', 'url' => ['/site/index']],
+        'links' => [
+            ['label' => 'Histórico de Cervejas', 'url' => null],
+        ],
+        'options' => ['class' => 'breadcrumb', 'style' => 'background-color: #f7f5f0; margin-bottom: -8px;'],
+        'itemTemplate' => "<li style='display: inline-block; margin-right: 5px;'>{link}</li> &nbsp; > &nbsp; ",
+        'activeItemTemplate' => "<li style='display: inline-block; margin-right: 5px; font-weight: bold;'>{link}</li>",
+    ]); ?>
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= GridView::widget([
@@ -23,8 +34,18 @@ $this->params['breadcrumbs'][] = $this->title;
         [
             'label' => 'Top Bebidas',
             'value' => function ($model, $key, $index, $widget) {
-                return $index + 1;
+                switch ($index) {
+                    case 0:
+                        return '<i class="fa fa-trophy" style="color: gold; font-size: 18px;"></i>';
+                    case 1:
+                        return '<i class="fa fa-trophy" style="color: #939393; font-size: 18px;"></i>';
+                    case 2:
+                        return '<i class="fa fa-trophy" style="color: #cd7f32; font-size: 18px;"></i>';
+                    default:
+                        return $index + 1;
+                }
             },
+            'format' => 'raw',
             'headerOptions' => ['style' => 'text-align: center; font-weight: bold;  background-color: #1e1e1e; color: #e7e7e7'],
             'contentOptions' => [
             'style' => 'text-align: center; width: 50px; font-weight: bold; color: white; padding: 10px 0 10px 0; font-size: 16px;'
@@ -44,4 +65,27 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 </div>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap');
 
+h1{
+    font-family: "Space Grotesk", sans-serif;
+    display: flex;
+    justify-content: center;
+    padding-left: 15px;
+    padding-right: 15px;
+    font-weight: bold;
+}
+
+.breadcrumb {
+    font-size: 14px;
+    color: #333;
+}
+.breadcrumb a {
+    text-decoration: none;
+    color: #373737;
+}
+.breadcrumb a:hover {
+    text-decoration: none;
+}
+</style>
