2003341191912da6d36f76e21d871cae0c4cd633
Abner Gonçalves <114529700+AbnerTesP@users.noreply.github.com>
2025-01-08 20:35:02 +0000

    Teste de aceitação completo

--- LupsyWeb/frontend/tests/_support/AcceptanceTester.php
+++ LupsyWeb/frontend/tests/_support/AcceptanceTester.php
@@ -26,18 +26,4 @@ class AcceptanceTester extends \Codeception\Actor
     /**
      * Define custom actions here
      */
-
-    public function login($username, $password)
-    {
-        $I = $this;
-        $I->amOnPage('/index.php?r=site%2Flogin');
-        $I->fillField('Username', $username);
-        $I->fillField('Password', $password);
-        $I->click('login-button');
-        $I->wait(2);
-        $I->click('Cerveja');
-        $I->wait(2);
-        $I->click('Skol');
-        $I->click('login-button');
-    }
 }
--- LupsyWeb/frontend/tests/acceptance/loginCest.php
+++ LupsyWeb/frontend/tests/acceptance/loginCest.php
@@ -7,9 +7,29 @@ use yii\helpers\Url;
 
 class loginCest
 {
-    public function login(AcceptanceTester $I)
+    public function tchimCerverja(AcceptanceTester $I)
     {
-        $I->login('admin', 'Admin123');
+        $I->amOnPage('/index.php?r=site%2Flogin');
+        $I->fillField('Username', 'cliente');
+        $I->fillField('Password', 'Cliente123');
+        $I->see('Entrar');
+        $I->click('login-button');
         $I->wait(2);
+        $I->click('Cerveja');
+        $I->see('Sagres');
+        $I->wait(2);
+        $I->click('Sagres');
+        $I->wait(2);
+        $I->seeElement('.fa.fa-heart');
+        $I->click('.fa.fa-heart');
+        $I->wait(2);
+        $I->see('TCHIM-TCHIM');
+        $I->click('TCHIM-TCHIM');
+        $I->wait(5);
+        $I->acceptPopup(); // Aceita o alerta (ou use $I->dismissPopup() para dispensar o alerta)
+        $I->wait(10);
+        $I->see('Favoritos');
+        $I->click('Favoritos');
+        $I->wait(5);
     }
 }
